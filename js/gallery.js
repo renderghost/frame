75110f0f39d2e572e08ea556b729eefb2abab2cc
Barry Prendergast <barry.m.prendergast@gmail.com>
2024-02-21 22:36:02 +0100

    chore: improve image thumbnail interactivity and update photo specifications table
    - Create a div wrapper for each thumbnail to make it more interactive
    - Add attributes to the thumbnail wrapper for accessibility
    - Update event listener for the thumbnail wrapper to load the photo in the showcase
    - Add a new CSS class for the thumbnail wrapper
    - Update the grid container to append the thumbnail wrapper instead of the image directly
    - Update the photo specifications table by removing the date row and adding a title for the table
    - Update the class name for the viewer title in the photo specifications table

--- js/gallery.js
+++ js/gallery.js
@@ -7,14 +7,22 @@ document.addEventListener('DOMContentLoaded', function () {
         .then(response => response.json())
         .then(data => {
             data.forEach((photo, index) => {
-                // Create and append the thumbnail to the grid
-                const img = document.createElement('img');
-                img.src = `photos/${photo.filename}`; // Assuming thumbnails and photos are in the same path
-                img.alt = `Thumbnail for ${photo.camera}`;
-                img.className = 'thumbnail';
-                img.addEventListener('click', () => loadPhotoToShowcase(photo));
+                // Create a div to wrap the image and make it more interactive
+                const thumbnailWrapper = document.createElement('div');
+                thumbnailWrapper.className = 'thumbnail';
+                thumbnailWrapper.setAttribute('role', 'button');
+                thumbnailWrapper.setAttribute('tabindex', '0');
+                thumbnailWrapper.setAttribute('alt', `Thumbnail for ${photo.camera}`);
+                thumbnailWrapper.addEventListener('click', () => loadPhotoToShowcase(photo));
 
-                gridContainer.appendChild(img);
+                // Create and append the thumbnail image to the wrapper
+                const img = document.createElement('img');
+                img.src = `photos/${photo.filename}`;
+                img.alt = `Thumbnail for ${photo.camera}`;
+                img.className = 'thumbnail-content';
+
+                thumbnailWrapper.appendChild(img);
+                gridContainer.appendChild(thumbnailWrapper);
 
                 // Automatically load the first photo into the showcase
                 if (index === 0) {
@@ -41,20 +49,18 @@ document.addEventListener('DOMContentLoaded', function () {
 
         // HTML string for table rows
         const tableHtml = `
-        <h2 class="viewer-title">Specifications</h2>
-        <tr class="row"><td class="meta-label">Date:</td><td class="meta-value">${photo.date}</td></tr>
-        <tr class="row"><td class="meta-label">Camera:</td><td class="meta-value">${photo.camera}</td></tr>
-        <tr class="row"><td class="meta-label">Exposure:</td><td class="meta-value">${photo.exposure}</td></tr>
-        <tr class="row"><td class="meta-label">Aperture:</td><td class="meta-value">f/${parseFloat(photo.aperture.split('/')[0]) / parseFloat(photo.aperture.split('/')[1])}</td></tr>
-        <tr class="row"><td class="meta-label">ISO:</td><td class="meta-value">${photo.iso}</td></tr>
-        <tr class="row"><td class="meta-label">Focal Length:</td><td class="meta-value">${parseFloat(photo.focal_length.split('/')[0]) / parseFloat(photo.focal_length.split('/')[1])}mm</td></tr>
-        <tr class="row"><td class="meta-label">Flash:</td><td class="meta-value">${photo.flash}</td></tr>
-        <tr class="row"><td class="meta-label">Orientation:</td><td class="meta-value">${photo.orientation}</td></tr>
-        <tr class="row"><td class="meta-label">Dimensions:</td><td class="meta-value">${photo.image_width}x${photo.image_height}</td></tr>
-    `;
+            <h2 class="viewer-title">Specification</h2>
+            <tr class="row"><td class="meta-label">Camera:</td><td class="meta-value">${photo.camera}</td></tr>
+            <tr class="row"><td class="meta-label">Exposure:</td><td class="meta-value">${photo.exposure}</td></tr>
+            <tr class="row"><td class="meta-label">Aperture:</td><td class="meta-value">f/${parseFloat(photo.aperture.split('/')[0]) / parseFloat(photo.aperture.split('/')[1])}</td></tr>
+            <tr class="row"><td class="meta-label">ISO:</td><td class="meta-value">${photo.iso}</td></tr>
+            <tr class="row"><td class="meta-label">Focal Length:</td><td class="meta-value">${parseFloat(photo.focal_length.split('/')[0]) / parseFloat(photo.focal_length.split('/')[1])}mm</td></tr>
+            <tr class="row"><td class="meta-label">Flash:</td><td class="meta-value">${photo.flash}</td></tr>
+            <tr class="row"><td class="meta-label">Orientation:</td><td class="meta-value">${photo.orientation}</td></tr>
+            <tr class="row"><td class="meta-label">Dimensions:</td><td class="meta-value">${photo.image_width}x${photo.image_height}</td></tr>
+        `;
 
         table.innerHTML = tableHtml;
         metaViewContainer.appendChild(table);
     }
-
 });
